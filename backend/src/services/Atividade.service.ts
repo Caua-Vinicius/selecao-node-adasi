2e17984e68dc7f2c988a3b23a0b9a7ad369cbaf6
Cauã Vinicius Gomes Monteiro <cauavinicius.contato@gmail.com>
2024-04-01 02:11:02 -0300

    Implementação das regras de Agendamento

--- backend/src/services/Atividade.service.ts
+++ backend/src/services/Atividade.service.ts
@@ -1,6 +1,10 @@
 import { PrismaService } from 'src/prisma.service';
 import { Atividade, Prisma } from '@prisma/client';
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { AtividadeUpdateDto } from 'src/Dtos/Atividade-Update.dto';
 
 @Injectable()
@@ -40,12 +44,76 @@ export class AtividadeService {
   }
 
   async startAtividade(id: string, horaInicio: string): Promise<Atividade> {
+    // Verificar a tolerância de 15 minutos para mais ou para menos
+    const toleranciaMinutos = 15;
+    const horaInicioAtividade = new Date(horaInicio);
+    const Atividade = await this.prisma.atividade.findUnique({
+      where: { id: String(id) },
+    });
+
+    //Verifica se a Atividade já foi iniciada e se existe
+
+    if (!Atividade) {
+      throw new NotFoundException('Atividade não encontrada');
+    }
+    if (Atividade.horaInicio) {
+      throw new BadRequestException('Você já começou a atividade!');
+    }
+
+    const diferencaMinutos =
+      (horaInicioAtividade.getTime() -
+        Atividade.horaAgendamentoInicio.getTime()) /
+      (1000 * 60);
+
+    if (Math.abs(diferencaMinutos) > toleranciaMinutos) {
+      throw new BadRequestException(
+        'A atividade só pode ser iniciada com uma tolerância de 15 minutos para mais ou para menos.',
+      );
+    }
+
     return this.prisma.atividade.update({
       where: { id },
-      data: { horaInicio: new Date(horaInicio) },
+      data: { horaInicio: horaInicioAtividade },
     });
   }
+
   async endAtividade(id: string, horaTermino: string): Promise<Atividade> {
+    const Atividade = await this.prisma.atividade.findUnique({
+      where: { id },
+    });
+
+    if (!Atividade) {
+      throw new NotFoundException('Atividade não encontrada');
+    }
+    if (Atividade.horaTermino) {
+      throw new BadRequestException('Atividade já finalizada');
+    }
+    if (!Atividade.horaInicio) {
+      throw new BadRequestException(
+        'Não é possivel finalizar uma atividade que não foi começada',
+      );
+    }
+
+    const horaInicio = Atividade.horaInicio;
+    const horaTerminoAtividade = new Date(horaTermino);
+    const SeisHorasMilisegundos = 6 * 60 * 60 * 1000;
+
+    if (horaTerminoAtividade < horaInicio) {
+      throw new BadRequestException(
+        'A data e hora de término não podem ser anteriores à data e hora de início.',
+      );
+    }
+
+    const duracaoAtividade =
+      horaTerminoAtividade.getTime() - horaInicio.getTime();
+
+    if (duracaoAtividade > SeisHorasMilisegundos) {
+      // 6 horas em milissegundos
+      throw new BadRequestException(
+        'A duração da atividade não pode ultrapassar 6 horas.',
+      );
+    }
+
     return this.prisma.atividade.update({
       where: { id },
       data: { horaTermino: new Date(horaTermino) },
